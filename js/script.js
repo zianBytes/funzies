dcdca715b1c7ff1ae543bcb3668ea06021f04b58
zianBytes <mohammedzianv@gmail.com>
2025-08-04 02:29:29 -0400

    Just built this simple basic arithmetic calculator without any help from AI

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// Addition Functionality: 
+
+document.getElementById("add-calculate").addEventListener("click", function() {
+    const num1 = parseFloat(document.getElementById("add-num1").value);
+    const num2 = parseFloat(document.getElementById("add-num2").value);
+    
+    // Validate Inputs with a If Statement
+    if( !isNaN(num1) && !isNaN(num1)){
+        const totaladd = num1 + num2;
+
+        // Update:
+        document.getElementById("add-result").textContent = totaladd;
+        document.getElementById("add-num1").value = "";
+        document.getElementById("add-num2").value = "";
+
+    } else {
+        document.getElementById("add-result").textContent = "0";
+    }
+})
+
+// Substraction Functionality:
+
+document.getElementById("sub-calculate").addEventListener("click", function(){
+    const num1 = parseFloat(document.getElementById("sub-num1").value);
+    const num2 = parseFloat(document.getElementById("sub-num2").value);
+
+    // Validate Inputs with if statement
+    if( !isNaN(num1) && !isNaN(num2)){
+        const totalSub = num1-num2;
+
+        //Update:
+        document.getElementById("sub-result").textContent = totalSub;
+        document.getElementById("sub-num1").value = "";
+        document.getElementById("sub-num2").value = "";
+    }else {
+        document.getElementById("sub-result").textContent = "0";
+    }
+})
+
+// Multiplication Functionality:
+
+document.getElementById("mul-calculate").addEventListener("click", function(){
+    const num1 = parseFloat(document.getElementById("mul-num1").value);
+    const num2 = parseFloat(document.getElementById("mul-num2").value);
+
+    // Validate Inputs with if statement
+    if( !isNaN(num1) && !isNaN(num2)){
+        const totalMul = num1*num2;
+
+        //Update:
+        document.getElementById("mul-result").textContent = totalMul;
+        document.getElementById("mul-num1").value = "";
+        document.getElementById("mul-num2").value = "";
+    }else {
+        document.getElementById("mul-result").textContent = "0";
+    }
+})
+
+// Division Functionality:
+
+document.getElementById("div-calculate").addEventListener("click", function(){
+    const num1 = parseFloat(document.getElementById("div-num1").value);
+    const num2 = parseFloat(document.getElementById("div-num2").value);
+
+    // Validate Inputs with if statement
+    if( !isNaN(num1) && !isNaN(num2)){
+        const totalDiv = num1/num2;
+
+        //Update:
+        document.getElementById("div-result").textContent = totalDiv;
+        document.getElementById("div-num1").value = "";
+        document.getElementById("div-num2").value = "";
+    }else {
+        document.getElementById("div-result").textContent = "0";
+    }
+})
